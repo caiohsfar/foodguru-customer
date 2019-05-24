98b58c2f8d2d0378c7a1c79438c9612d9855c9fd
Anderson dos Santos <andersoon_12@hotmail.com>
2019-05-23 23:36:48 -0300

    mapa add location

--- android/app/src/main/java/com/customer/MainApplication.java
+++ android/app/src/main/java/com/customer/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -14,6 +15,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // <-- Add this line
+import com.airbnb.android.react.maps.MapsPackage;
 
 
 import java.util.Arrays;
@@ -35,10 +37,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new FBSDKPackage(mCallbackManager),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
-            new RNFirebaseAuthPackage()
+            new RNFirebaseAuthPackage(),
+            new MapsPackage()
       );
     }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,209 @@
 import React, { Component } from 'react';
-import firebase from 'react-native-firebase';
-import { LoginManager } from 'react-native-fbsdk';
-import {
-  View, Text, Image, Button
-} from 'react-native';
-import styles from './styles';
+import { ScrollView, View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
 import api from '../../services/api';
 import { getIdToken } from '../../services/userService';
 
-export default class Home extends Component {
-  logout = () => {
-    firebase.auth().signOut();
-    LoginManager.logOut();
-    this.props.navigation.navigate('Auth');
+export default class App extends Component {
+  state = {
+    //latitude: null, 
+    //longitude: null,
+    result: null,
+    places: [
+      {
+        id: 1,
+        title: 'Yoki Galetos',
+        description: 'Torrões',
+        latitude: -8.062283,
+        longitude: -34.932594,
+      },
+      {
+        id: 2,
+        title: 'Arena Camarão',
+        description: 'Cordeiro',
+        latitude: -8.060282,
+        longitude: -34.928128,
+      },
+      {
+        id: 3,
+        title: 'Coni Móvel Temakeria',
+        description: 'Soledade',
+        latitude: -8.056676,
+        longitude: -34.892925,
+      },
+      {
+        id: 4,
+        title: 'Cachacaria Tradição',
+        description: 'Graças',
+        latitude: -8.044604,
+        longitude: -34.898989,
+      }
+    ]
   };
 
-  requestExemplo = async () => {
+  /*requestExemplo = async () => {
     const token = await getIdToken();
     // Tem sempre que setar esse token pra validar a requisição
     api.defaults.headers.common['x-access-token'] = token;
     try {
-      const result = await api.get('/api/customers');
-      console.log('RESULT', result);
+      const response = await api.get('/establishments');
+      this.setState({places:response.data}),
+      console.log(result);
     } catch (e) {
-      console.log('RESULT', e);
+      console.log(e);
     }
+  };*/
+
+  /*componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      ({coords: {latitude, longitude}}) => 
+        this.setState({ latitude,longitude}),
+      (error) => { console.log(error); },
+      { enableHighAccuracy: true, timeout: 30000 }
+    ),
+  }*/
+
+  _mapReady = () => {
+    this.state.places[0].mark.showCallout();
   };
 
   render() {
+    const {latitude, longitude} = this.state.places[0];
+    //const  {latitude, longitude} = this.state;
+   
+
     return (
       <View style={styles.container}>
-        <Image style={styles.image} source={{ uri: firebase.auth().currentUser.photoURL }} />
-        <Text>{firebase.auth().currentUser.email}</Text>
-        <Button onPress={this.logout} title="LogOut" />
+        <MapView
+          ref={map => this.mapView = map}
+          showsUserLocation = {true}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0015,
+            longitudeDelta: 0.0121,
+          }}
+          style={styles.mapView}
+          //rotateEnabled={false}
+          //scrollEnabled={false}
+          //zoomEnabled={false}
+          showsPointsOfInterest={false}
+          showBuildings={false}
+          onMapReady={this._mapReady}
+        >
+          { this.state.places.map(place => (
+            <MapView.Marker
+              ref={mark => place.mark = mark}
+              title={place.title}
+              description={place.description}
+              key={place.id}
+              coordinate={{
+                latitude: place.latitude,
+                longitude: place.longitude,
+              }}
+            />
+          ))}
+        </MapView>
+        <ScrollView
+          style={styles.placesContainer}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onMomentumScrollEnd={(e) => {
+            const place = (e.nativeEvent.contentOffset.x > 0)
+              ? e.nativeEvent.contentOffset.x / Dimensions.get('window').width
+              : 0;
+
+            const { latitude, longitude, mark } = this.state.places[place];
+
+            this.mapView.animateToCoordinate({
+              latitude,
+              longitude
+            }, 500);
+
+            setTimeout(() => {
+              mark.showCallout();
+            }, 500)
+          }}
+        >
+          { this.state.places.map(place => (
+            <View key={place.id} style={styles.place}>
+              <Text style={styles.title}>{ place.title }</Text>
+              <Text style={styles.description}>{ place.description }</Text>
+              <TouchableOpacity onPress={this.props.Cardapio} style={styles.buttonMenu}>
+                <View style={styles.menu}>
+                  <Text style={styles.menuFont}>Cardápio</Text>
+                </View>
+              </TouchableOpacity>
+              
+            </View>
+          )) }
+        </ScrollView>
       </View>
     );
   }
 }
+
+const { height, width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+
+  mapView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  placesContainer: {
+    width: '100%',
+    maxHeight: 150,
+  },
+  place: {
+    width: width - 40,
+    maxHeight: 150,
+    backgroundColor: '#FFF',
+    marginHorizontal: 20,
+    marginHorizontal: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    backgroundColor: 'transparent',
+  },
+  description: {
+    textAlign: 'center',
+    color: '#999',
+    fontSize: 12,
+    marginTop: 5,
+  },
+  buttonMenu: {
+    marginTop: 10,
+  },
+  menu: {
+    //flex: 0.5,
+    //flexDirection: 'row',
+    width: width -80,
+    height: 30,
+    //paddingVertical: 20,
+    //padding: 20,
+    backgroundColor: '#cd170c',
+    borderRadius: 3,
+  },
+  menuFont:{
+    textAlign: 'center',
+    //padding: 20,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fff'
+  },
+});
